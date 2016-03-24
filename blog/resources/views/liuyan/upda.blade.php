adf76e37187939546c9811fce617b16fc99beb99
sunyiqi <906331318@qq.com>
2016-03-24 13:42:21 +0800

    del

--- blog/resources/views/liuyan/upda.blade.php
+++
@@ -1,5 +0,0 @@
-<form method="post" action="/xiu">
-	<input type="hidden" name="l_id" value="{{$arr->l_id}}">
-	<textarea name="contents" rows="10" cols="20">{{$arr->l_contents}}</textarea>
-	<input type="submit" value="提交">
-</form>
